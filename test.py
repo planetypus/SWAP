d58343c7650ba76ce3a0de73110af8f85a380f38
planetypus <planetypus@yahoo.com>
2015-07-19 20:18:35 +0900

    new python file

---
+++ test.py
@@ -0,0 +1,16 @@
+#This is to learn variables
+x = 1/4
+y = x + 21
+print (y)
+
+#program to calculate my age
+
+current_year=2015
+birth_date=1984
+age = current_year - birth_date
+print (age)
+print ("This year, my age is %d" %age)
+
+print ("I was born in %d, and my age is %d" %(birth_date, age))
+line1 = "I was born in %d, and my age is %d" %(birth_date, age)
+print (line1)
